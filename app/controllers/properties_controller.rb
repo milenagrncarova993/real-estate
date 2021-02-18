e1c71983588f6ec706ff122d8b5238d505cb16d6
Milena Grncarova <milenagrncarov@outlook.com>
2021-02-18 23:10:46 +0100

    Worked on the create new property form

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,28 +4,40 @@ class PropertiesController < ApplicationController
     @properties = Property.all
   end
     
+  def show
+    @property = Property.find(params[:id])
+  end
 
-    def show
-    end
-
-    def new
-    end
+  def new
+    @property = Property.new
+  end
     
+  def create
+    @property = Property.new(property_params)
+    @property.save
+    redirect_to properties_path(@property)
+  end
 
+  def edit
+    @property = Property.find(params[:id])
+  end
 
+  def update
+    @property = Property.find(params[:id])
+    @property.update(property_params)
+    redirect_to properties_path(@property)
+  end
 
+  def destroy
+    @property = Property.find(params[:id])
+    @property.destroy
+    redirect_to properties_path
+  end
 
+  private
 
-
-
-
-
-
-
-
-
-
-
-
+  def property_params
+    params.require(:property).permit(:address, :size, :year, :rooms, :price, :state, :type_of_property)
+  end
 
 end
