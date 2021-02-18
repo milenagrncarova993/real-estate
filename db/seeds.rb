e5831fc7086367fbacab8b080dd9597924fd84bf
Milena Grncarova <milenagrncarov@outlook.com>
2021-02-18 15:39:39 +0100

    Created some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database now...'
+Booking.destroy_all
+Property.destroy_all
+User.destroy_all
+puts 'Database clean ✅'
+
+# Users:
+
+owner_array = []
+
+user_owner = User.new
+user_owner.email = 'anne.owner@gmail.com' # user_owner1 to offer trucks
+user_owner.password = '123456'
+user_owner.save!
+
+
+property_array = []
+
+p = Property.new
+p.address = "Wedekind Strasse 23"
+p.size = "50m2"
+p.year = 1990
+p.rooms = 2
+p.price = 1000
+p.type = "appartment"
+p.state = "simple"
+p.user = user_owner
+p.save 
+
+property_array << p
+
+
+booking_array = []
+
+b = Booking.new
+b.start_date = DateTime.new(2020, 11, 18)
+b.end_date = DateTime.new(2020, 11, 20)
+b.status = "Free"
+b.user = user_owner
+b.property = p
+b.save
+
+booking_array << b 
+
+puts "Done!"
+puts "Created #{User.count} users."
+puts "Created #{Property.count} properties."
+puts "Created #{Booking.count} bookings."
