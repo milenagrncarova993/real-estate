bbb5fcda7658dc114217203e44f650d2e0f2cc86
Milena Grncarova <milenagrncarov@outlook.com>
2021-02-25 18:25:14 +0100

    Created a path to the contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
+  get "contact", to: "pages#contact"
 
   resources :properties do
     resources :bookings, only: [ :new, :create ]
