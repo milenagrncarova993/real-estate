76e050542a752a1b1db4ea5426c48b62a90684a2
Milena Grncarova <milenagrncarov@outlook.com>
2021-02-26 10:13:42 +0100

    geocode

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -7,7 +7,7 @@ class PropertiesController < ApplicationController
       {
         lat: property.latitude,
         lng: property.longitude,
-        infoWindow: render_to_string(partial: "infowindow", locals: { property: property }),
+        infoWindow: render_to_string(partial: "info_window", locals: { property: property }),
         image_url: helpers.asset_url('pickup.svg')
       }
     end
